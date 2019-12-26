dcc65e2db3793c9cff87358f47dd2a67829da6de
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-12-26 18:10:31 +0700

    6366

--- ApiProject5/ParameterSetValue/ParameterValueHandler.cs
+++ ApiProject5/ParameterSetValue/ParameterValueHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Autodesk.Revit.UI;
 namespace ApiProject5.ParameterSetValue
@@ -10,12 +11,43 @@ namespace ApiProject5.ParameterSetValue
     {
         public void Execute(UIApplication app)
         {
-            throw new NotImplementedException();
+            bool elementInProject = AppPanalParameterSet.myFormParameterSet.radioButtonInEntireProject.Checked;
+            string typeText = Constant.Normal;
+            if (AppPanalParameterSet.myFormParameterSet.radioButtonLowerText.Checked) typeText = Constant.Lower;
+            else if (AppPanalParameterSet.myFormParameterSet.radioButtonSentenceText.Checked) typeText = Constant.Sentense;
+            else if (AppPanalParameterSet.myFormParameterSet.radioButtonTitleText.Checked) typeText = Constant.Title;
+            else if (AppPanalParameterSet.myFormParameterSet.radioButtonUpperText.Checked) typeText = Constant.Upper;
+
+
         }
 
         public string GetName()
         {
             return "ParameterSetValue";
+        }
+        private string SetTextFormat(string typeFormat,string textValue)
+        {
+            textValue = Regex.Replace(textValue, @"^\s+", "");
+            string stringFormat = textValue;
+            switch (typeFormat)
+            {
+                case Constant.Normal:
+                    stringFormat = textValue;
+                    break;
+                case Constant.Upper:
+                    stringFormat = textValue.ToUpper();
+                    break;
+                case Constant.Lower:
+                    stringFormat = textValue.ToLower();
+                    break;
+                case Constant.Sentense:
+                    stringFormat= textValue.First().ToString().ToUpper() + textValue.Remove(0, 1).ToLower();
+                    break;
+                case Constant.Title:
+                    stringFormat= Regex.Replace(textValue, @"(^\w)|(\s\w)", m => m.Value.ToUpper());
+                    break;
+            }
+            return stringFormat;
         }
     }
 }
