ed0354a62f3cf9c22c0602c6f1782a78934929d1
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-12-26 19:25:53 +0700

    6253

--- ApiProject5/ParameterSetValue/ParameterValueHandler.cs
+++ ApiProject5/ParameterSetValue/ParameterValueHandler.cs
@@ -5,20 +5,81 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using ApiProject5.Helper;
+
 namespace ApiProject5.ParameterSetValue
 {
     public class ParameterValueHandler : IExternalEventHandler
     {
         public void Execute(UIApplication app)
         {
+            Document doc = app.ActiveUIDocument.Document;
             bool elementInProject = AppPanalParameterSet.myFormParameterSet.radioButtonInEntireProject.Checked;
             string typeText = Constant.Normal;
+            string valueSet = AppPanalParameterSet.myFormParameterSet.textBoxValueParameterEle.Text;
+            string nameParameter = AppPanalParameterSet.myFormParameterSet.comboBoxNameParameterEle
+                .GetItemText(AppPanalParameterSet.myFormParameterSet.comboBoxNameParameterEle.SelectedItem);
             if (AppPanalParameterSet.myFormParameterSet.radioButtonLowerText.Checked) typeText = Constant.Lower;
             else if (AppPanalParameterSet.myFormParameterSet.radioButtonSentenceText.Checked) typeText = Constant.Sentense;
             else if (AppPanalParameterSet.myFormParameterSet.radioButtonTitleText.Checked) typeText = Constant.Title;
             else if (AppPanalParameterSet.myFormParameterSet.radioButtonUpperText.Checked) typeText = Constant.Upper;
-
-
+            var allSelectionIds = app.ActiveUIDocument.Selection.GetElementIds();
+            Element elementFirst = doc.GetElement(allSelectionIds.First());
+            var listParameters = elementFirst.Parameters;
+            List<Element> listElementSetParameter = new List<Element>();           
+            if (elementInProject)
+            {
+                ElementCategoryFilter filter = new ElementCategoryFilter(elementFirst.Category.Id);
+                listElementSetParameter = new FilteredElementCollector(doc).WhereElementIsNotElementType().WherePasses(filter).ToList();
+            }
+            else
+            {
+                foreach(ElementId id in allSelectionIds)
+                {
+                    Element el = doc.GetElement(id);
+                    listElementSetParameter.Add(el);
+                }
+            }
+            foreach(Element elementSet in listElementSetParameter)
+            {
+                Parameter paraSet = null;
+                try
+                {
+                    paraSet = elementSet.LookupParameter(nameParameter);
+                }
+                catch { continue; }
+                if (paraSet != null)
+                {
+                    string newString = string.Empty;
+                    if(!string.IsNullOrEmpty(valueSet)&&valueSet!=" ")
+                    {
+                        newString = SetTextFormat(typeText, valueSet);
+                    }
+                    else
+                    {
+                        string oldString = ParameterRevit.ParameterToString(paraSet);
+                        newString = SetTextFormat(typeText,oldString);
+                    }
+                   using(Transaction t= new Transaction(doc, "SetParameterValue1"))
+                    {
+                        t.Start();
+                        try
+                        {
+                            if (paraSet.StorageType == StorageType.Integer) paraSet.Set(int.Parse(newString));
+                            else if (paraSet.StorageType == StorageType.Double) paraSet.SetValueString(newString);
+                            //else if (paraSet.StorageType == StorageType.ElementId) paraSet.SetValueString(newString);
+                            else if (paraSet.StorageType == StorageType.String) paraSet.Set(newString);
+                            t.Commit();
+                        }
+                        catch
+                        {
+                            t.Commit();
+                        }
+                    }
+                }
+                
+            }
         }
 
         public string GetName()
