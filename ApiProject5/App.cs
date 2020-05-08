49aa97389dd1405531328dfd16e836192c8f4491
NGUYENVANPHONG <ngvanphong2012@gmail.com>
2020-05-08 11:53:56 +0900

    6789

--- ApiProject5/App.cs
+++ ApiProject5/App.cs
@@ -1,9 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: ApiProject5.App
-// Assembly: ApiProject5, Version=2019.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: C3D4D4B7-6EF2-4F20-A28D-362DDA07E142
-// Assembly location: C:\Program Files\Autodesk\Revit 2019\AddIns\ApiProject5.dll
-
+﻿
 using ApiProject5.Button;
 using Autodesk.Revit.UI;
 
--- ApiProject5/DuctPipe/StatisAngleViewer.cs
+++ ApiProject5/DuctPipe/StatisAngleViewer.cs
@@ -1,13 +1,8 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: ApiProject5.DuctPipe.StatisAngleViewer
-// Assembly: ApiProject5, Version=2019.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: C3D4D4B7-6EF2-4F20-A28D-362DDA07E142
-// Assembly location: C:\Program Files\Autodesk\Revit 2019\AddIns\ApiProject5.dll
-
+﻿
 namespace ApiProject5.DuctPipe
 {
   public class StatisAngleViewer
   {
-    public static string StatisViewerShow = (string) null;
+    public static string StatisViewerShow =  null;
   }
 }
